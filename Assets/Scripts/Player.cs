be7ace5295a9b2be4c8e5209ad856bd9b1dc88f2
Urbn Bear <tgsauers@gmail.com>
2023-05-24 16:33:03 -0700

    created player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,7 +8,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // take the current position = new posistion (0, 0, 0)
+        object rotation = Transform.rotation;
     }
 
     // Update is called once per frame
